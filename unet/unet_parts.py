b642250d7bfa3aca6854af880cf6be4dd842d7d1
Peng Bo <pengbo0054@gmail.com>
2018-08-27 20:10:43 +0800

    update unet

--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -71,7 +71,7 @@ class up(nn.Module):
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         else:
-            self.up = nn.ConvTranspose2d(in_ch//2, in_ch//2, 2, stride=2, padding=1)
+            self.up = nn.ConvTranspose2d(in_ch//2, in_ch//2, 2, stride=2)
 
         self.conv = double_conv_up(in_ch, out_ch)
 
