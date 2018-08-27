6eceac50c0dd55638cca8f50cd1afda697301276
Peng Bo <pengbo0054@gmail.com>
2018-08-27 23:46:30 +0800

    update

--- submit.py
+++ submit.py
@@ -28,4 +28,4 @@ def submit(net, gpu=False):
 if __name__ == '__main__':
     net = UNet(3, 1).cuda()
     net.load_state_dict(torch.load('./checkpoints/CP1.pth'))
-    submit(net, False)
+    submit(net, True)
