a367c0c5f4e723d685e12f2f75aff7258e22425b
Peng Bo <pengbo0054@gmail.com>
2018-08-27 17:59:17 +0800

    update unet

--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -10,12 +10,12 @@ class double_conv(nn.Module):
     def __init__(self, in_ch, out_ch):
         super(double_conv, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, 3, padding=0),
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
             nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True),
             nn.Conv2d(out_ch, out_ch, 3, padding=1),
             nn.BatchNorm2d(out_ch),
-            nn.ReLU(inplace=True)
+            nn.ReLU()
         )
 
     def forward(self, x):
