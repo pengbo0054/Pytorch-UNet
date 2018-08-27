990dcf42816d59ef86efd450a31aa4ea8e7f2251
Peng Bo <pengbo0054@gmail.com>
2018-08-27 21:15:08 +0800

    update unet

--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -72,7 +72,7 @@ class up(nn.Module):
 class outconv(nn.Module):
     def __init__(self, in_ch, out_ch):
         super(outconv, self).__init__()
-        self.conv = nn.Conv2d(in_ch, out_ch, 1, padding=1)
+        self.conv = nn.Conv2d(in_ch, out_ch, 1)
 
     def forward(self, x):
         x = self.conv(x)
