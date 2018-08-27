60d964d4f66acdbbd1a4c24b0fe963666157eceb
Peng Bo <pengbo0054@gmail.com>
2018-08-27 21:57:58 +0800

    update unet

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -26,5 +26,8 @@ class UNet(nn.Module):
         x = self.up2(x, x3)
         x = self.up3(x, x2)
         x = self.up4(x, x1)
+        print(x.shape)
         x = self.outc(x)
+        print(x.shape)
+
         return x
