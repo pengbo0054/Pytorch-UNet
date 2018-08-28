48da5f1baed084754c3d7feed94090674aa89b0a
Peng Bo <pengbo0054@gmail.com>
2018-08-28 17:43:01 +0800

    update

--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -58,15 +58,16 @@ class up(nn.Module):
             self.up = nn.ConvTranspose2d(in_ch//2, in_ch//2, 2, stride=2)
 
         self.conv = double_conv(in_ch, out_ch)
-
-    def forward(self, x1, x2, droprate):
+        self.droprate = droprate
+    
+    def forward(self, x1, x2):
         x1 = self.up(x1)
         diffX = x1.size()[2] - x2.size()[2]
         diffY = x1.size()[3] - x2.size()[3]
         x2 = F.pad(x2, (diffX // 2, int(diffX / 2),
                         diffY // 2, int(diffY / 2)))
         x = torch.cat([x2, x1], dim=1)
-        x = nn.Dropout(droprate)(x)
+        x = nn.Dropout(self.droprate)(x)
         x = self.conv(x)
         return x
 
