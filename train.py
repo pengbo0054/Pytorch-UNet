07347a416d241ba750c585cd86c1a288196c9120
Peng Bo <pengbo0054@gmail.com>
2018-08-29 11:57:06 +0800

    update

--- train.py
+++ train.py
@@ -77,7 +77,7 @@ def train_net(net,
             masks_pred = masks_pred.squeeze()
             #print(imgs.shape, masks_pred.shape)
             masks_probs = torch.sigmoid(masks_pred)
-            true_masks = true_masks != 0
+            true_masks = (true_masks != 0).astype(np.int32)
             masks_probs_flat = masks_probs.view(-1)
             
             true_masks_flat = true_masks.view(-1)
